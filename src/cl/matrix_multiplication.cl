ced2a5d8864f2e1e589f6b856a46fe93619af108
Makar Selivanov <makar.selivanov@gmail.com>
2022-10-13 01:58:10 +0300

    add mutliplication

--- src/cl/matrix_multiplication.cl
+++ src/cl/matrix_multiplication.cl
@@ -1,4 +1,98 @@
-__kernel void matrix_multiplication(...)
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+
+#define TILE_SIZE 16
+#define THREAD_WORK 2
+
+inline float getOrDefault(const __global float *arr, int i, int j, int n, int m) {
+    if (i < n && j < m)
+        return arr[i * m + j];
+    return 0;
+}
+
+inline void set(__global float *arr, int i, int j, int n, int m, int value) {
+    if (i < n && j < m) {
+        arr[i * m + j] = value;
+    }
+}
+
+__kernel void matrix_multiplication(
+        const __global float* a,
+        const __global float* b,
+        __global float* results,
+        const unsigned int m,
+        const unsigned int k,
+        const unsigned int n)
 {
-    // TODO
+    const unsigned int indn = get_global_id(0);   //from 0 to N with rounding
+    const unsigned int indm = get_global_id(1);   //from 0 to M with rounding
+    const unsigned int local_n = get_local_id(0); //from 0 to TILE_SIZE
+    const unsigned int local_m = get_local_id(1); //from 0 to TILE_SIZE
+    const unsigned int local_group_n = get_group_id(0);
+    const unsigned int local_group_m = get_group_id(1);
+    const unsigned int local_size_n = get_local_size(0);
+    const unsigned int local_size_m = get_local_size(1);
+    __local float suba[TILE_SIZE][TILE_SIZE];
+    __local float subb[TILE_SIZE][TILE_SIZE];
+    float result = 0.f;
+    for (unsigned int index = 0; index < (k + TILE_SIZE - 1); index += TILE_SIZE) {
+        const unsigned int shift = index;
+
+        suba[local_m][local_n] = getOrDefault(a, indm, shift + local_n, m, k);
+        subb[local_n][local_m] = getOrDefault(b, shift + local_m, indn, k, n);
+        barrier(CLK_LOCAL_MEM_FENCE);
+        for (unsigned int i = 0; i < TILE_SIZE; ++i) {
+            result += suba[local_m][i] * subb[local_n][i];
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+    set(results, indm, indn, m, n, result);
+}
+
+
+__kernel void matrix_multiplication_fma(
+        const __global float* a,
+        const __global float* b,
+        __global float* results,
+        const unsigned int m,
+        const unsigned int k,
+        const unsigned int n)
+{
+    const unsigned int indn = get_global_id(0);   //from 0 to N with rounding
+    const unsigned int indm = get_global_id(1);   //from 0 to M with rounding
+    const unsigned int local_n = get_local_id(0); //from 0 to TILE_SIZE
+    const unsigned int local_m = get_local_id(1); //from 0 to TILE_SIZE
+    const unsigned int local_group_n = get_group_id(0);
+    const unsigned int local_group_m = get_group_id(1);
+    const unsigned int local_size_n = get_local_size(0);
+    const unsigned int local_size_m = get_local_size(1);
+    __local float suba[TILE_SIZE][TILE_SIZE];
+    __local float subb[TILE_SIZE][TILE_SIZE];
+    float sum[THREAD_WORK];
+    for (unsigned int index = 0; index < THREAD_WORK; ++index)
+        sum[index] = 0;
+    const unsigned int new_local_n = local_n * THREAD_WORK;
+    for (unsigned int index = 0; index < (k + TILE_SIZE - 1); index += TILE_SIZE) {
+        const unsigned int shift = index;
+
+        for (unsigned int w = 0; w < THREAD_WORK; ++w)
+            suba[local_m][new_local_n + w] = getOrDefault(a, indm, shift + new_local_n + w, m, k);
+        for (unsigned int w = 0; w < THREAD_WORK; ++w)
+            subb[local_m][new_local_n + w] = getOrDefault(b, shift + local_m, indn * THREAD_WORK + w, k, n);
+
+        barrier(CLK_LOCAL_MEM_FENCE);
+        for (unsigned int i = 0; i < TILE_SIZE; ++i) {
+            float tmp = suba[local_m][i];
+            for (unsigned int w = 0; w < THREAD_WORK; ++w) {
+                sum[w] += tmp * subb[i][new_local_n + w];
+            }
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+    for (unsigned int w = 0; w < THREAD_WORK; ++w) {
+        set(results, indm, indn * THREAD_WORK + w, m, n, sum[w]);
+    }
 }
--- src/main_matrix_multiplication.cpp
+++ src/main_matrix_multiplication.cpp
@@ -19,8 +19,8 @@ int main(int argc, char **argv)
     context.init(device.device_id_opencl);
     context.activate();
 
-    int benchmarkingIters = 10; // TODO пока тестируетесь удобно выставить единицу
-    unsigned int M = 1024;
+    int benchmarkingIters = 1; // TODO пока тестируетесь удобно выставить единицу
+    unsigned int M = 1024; //1024
     unsigned int K = 1024;
     unsigned int N = 1024;
     const size_t gflops = ((size_t) M * K * N * 2) / (1000 * 1000 * 1000); // умножить на два, т.к. операция сложения и умножения
@@ -55,10 +55,9 @@ int main(int argc, char **argv)
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << gflops / t.lapAvg() << " GFlops" << std::endl;
     }
-
     const std::vector<float> cs_cpu_reference = cs;
 
-    /*
+
     gpu::gpu_mem_32f as_gpu, bs_gpu, cs_gpu;
     as_gpu.resizeN(M*K);
     bs_gpu.resizeN(K*N);
@@ -73,10 +72,10 @@ int main(int argc, char **argv)
     {
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
-            matrix_multiplication_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, bs_gpu, cs_gpu, M, K, N);
+            unsigned int work_group_size = 16;
+            unsigned int global_work_size1 = (N + work_group_size - 1) / work_group_size * work_group_size;
+            unsigned int global_work_size2 = (M + work_group_size - 1) / work_group_size * work_group_size;
+            matrix_multiplication_kernel.exec(gpu::WorkSize(work_group_size, work_group_size, global_work_size1, global_work_size2), as_gpu, bs_gpu, cs_gpu, M, K, N);
 
             t.nextLap();
         }
@@ -85,7 +84,6 @@ int main(int argc, char **argv)
     }
 
     cs_gpu.readN(cs.data(), M*N);
-    */
 
     // Проверяем корректность результатов
     double diff_sum = 0;
@@ -99,6 +97,50 @@ int main(int argc, char **argv)
     }
 
     double diff_avg = diff_sum / (M * N);
+    std::cout << "Average difference: " << diff_avg * 100.0 << "%" << std::endl;
+    if (diff_avg > 0.01) {
+        std::cerr << "Too big difference!" << std::endl;
+        return 1;
+    }
+
+    ocl::Kernel matrix_multiplication_kernel_fma(matrix_multiplication, matrix_multiplication_length, "matrix_multiplication_fma");
+
+    matrix_multiplication_kernel_fma.compile();
+    {
+        timer t;
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            unsigned int work_group_size = 16;
+            unsigned int thread_work = 2;
+            unsigned int thread_in_group = work_group_size / thread_work;
+            unsigned int global_work_size1 = (N + thread_in_group - 1) / thread_in_group * thread_in_group;
+            unsigned int global_work_size2 = (M + work_group_size - 1) / work_group_size * work_group_size;
+            matrix_multiplication_kernel_fma.exec(
+                    gpu::WorkSize(
+                            thread_in_group,
+                            work_group_size,
+                            global_work_size1,
+                            global_work_size2),
+                    as_gpu, bs_gpu, cs_gpu, M, K, N);
+
+            t.nextLap();
+        }
+        std::cout << "GPU fma: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "GPU fma: " << gflops / t.lapAvg() << " GFlops" << std::endl;
+    }
+
+    cs_gpu.readN(cs.data(), M*N);
+
+    diff_sum = 0;
+    for (int i = 0; i < M * N; ++i) {
+        double a = cs[i];
+        double b = cs_cpu_reference[i];
+        if (a != 0.0 && b != 0.0) {
+            double diff = fabs(a - b) / std::max(fabs(a), fabs(b));
+            diff_sum += diff;
+        }
+    }
+
+    diff_avg = diff_sum / (M * N);
     std::cout << "Average difference: " << diff_avg * 100.0 << "%" << std::endl;
     if (diff_avg > 0.01) {
         std::cerr << "Too big difference!" << std::endl;
